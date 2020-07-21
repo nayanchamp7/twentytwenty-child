0b3d16a815172c3017f798b5e788b23e74866f1b
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-21 15:03:00 +0600

    updated on scroll effect seo page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,37 @@
 (function($) {
     $('document').ready(function() {
 
-        console.log('hello pops');
-        if( $('body').has('.vx-seo-details-left-column') ) {
-            var seoDetailsTop = $('.vx-seo-details-left-column').offset().top;
-        
-            $(window).scroll(function() {
-
-                var currentScroll = $(window).scrollTop();
-                console.log(currentScroll);
-                if (currentScroll >= seoDetailsTop) {
-                    $('.vx-seo-sidebar').css({
-                        position: 'fixed',
-                        top: '0',
-                        right: '0'
-                    });
-                    
-                } else {
-                    $('.vx-seo-sidebar').css({ 
-                        position: 'static'
+        if ($(window).width() > 768) {
+            if( $('body').has('.vx-seo-sidebar') ) {
+                var seoDetailsTop = $('.vx-seo-sidebar').offset().top;
+                var nextAfterSeoDetails = $('.vx-seo-sidebar').next();
+            
+                $(window).scroll(function() {
+    
+                    var currentScroll = $(window).scrollTop();
+                    if (currentScroll >= seoDetailsTop) {
+                        $('.vx-seo-sidebar').css({
+                            position: 'fixed',
+                            top: '0',
+                            right: '0'
+                        });
                         
-                    });
-                }
+                    } else {
+                        $('.vx-seo-sidebar').css({ 
+                            position: 'static'
+                            
+                        });
+                    }
 
-            });    
+                    // when meet next div reset position
+                    if( currentScroll >= nextAfterSeoDetails ) {
+                        $('.vx-seo-sidebar').css({
+                            position: 'static'
+                        });
+                    }
+    
+                });    
+            }
         }
         
     });
