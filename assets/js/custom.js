09ac98a28033542a78863b1ebd70524431f9ce1a
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-21 15:52:21 +0600

    updated on scroll removed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,7 @@
 (function($) {
     $('document').ready(function() {
 
-        if ($(window).width() > 768) {
-            if( $(".vx-seo-sidebar").length > 0 ) {
-                var seoDetailsTop = $('.vx-seo-sidebar').offset().top;
-                var nextAfterSeoDetails = $('.vx-seo-sidebar').next();
-            
-                $(window).scroll(function() {
-    
-                    var currentScroll = $(window).scrollTop();
-                    if (currentScroll >= seoDetailsTop) {
-                        $('.vx-seo-sidebar').css({
-                            position: 'fixed',
-                            top: '0',
-                            right: '0'
-                        });
-                        
-                    } else {
-                        $('.vx-seo-sidebar').css({ 
-                            position: 'static'
-                            
-                        });
-                    }
-
-                    // when meet next div reset position
-                    if( currentScroll >= nextAfterSeoDetails ) {
-                        $('.vx-seo-sidebar').css({
-                            position: 'static'
-                        });
-                    }
-    
-                });    
-            }
-        }
+        
         
     });
 })(jQuery);
