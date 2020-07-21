2fa06bda2ad0dbca6143898ed3df5ab1a6943be8
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-21 13:58:20 +0600

    updated 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,25 +1,29 @@
 (function($) {
     $('document').ready(function() {
-        var seoDetailsTop = $('.vx--seo-details-left-column').offset().top;
-        
+
         console.log('hello pops');
-        $(window).scroll(function() {
+        if( $('body').has('.vx-seo-details-left-column') ) {
+            var seoDetailsTop = $('.vx-seo-details-left-column').offset().top;
+        
+            $(window).scroll(function() {
 
-            var currentScroll = $(window).scrollTop();
+                var currentScroll = $(window).scrollTop();
+                console.log(currentScroll);
+                if (currentScroll >= seoDetailsTop) {
+                    $('.vx-seo-sidebar').css({ 
+                        position: 'static'
+                        
+                    });
+                } else {
+                    $('.vx-seo-sidebar').css({
+                        position: 'fixed',
+                        top: '0',
+                        left: '0'
+                    });
+                }
 
-            if (currentScroll >= seoDetailsTop) {
-                $('.vx-seo-sidebar').css({ 
-                    position: 'static'
-                    
-                });
-            } else {
-                $('.vx-seo-sidebar').css({
-                    position: 'fixed',
-                    top: '0',
-                    left: '0'
-                });
-            }
-
-        });
+            });    
+        }
+        
     });
 })(jQuery);
