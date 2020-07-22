89c72caaa06ae2b2dc25285e9904370150d63e76
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-22 12:57:56 +0600

    testing sticky 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,47 +8,44 @@
         }
         
 
-        if( $(window).width() > 768 ) {
+        if( $(window).width() > 960 ) {
 
-            if( $(".vx-seo-details-left-column").length > 0 ) {
-
-                var seoDetailsTop = $('.vx-seo-details-left-column').offset().top;
-                var nextAfterSeoDetails = $('.vx-seo-sidebar').next();
-            
                 $(window).scroll(function() {
 
-                    if ( elementInView( $('.vx-seo-details-left-column').parent('.vx-seo-details-section').next() ) ) {
-                        //fire at will!
-                        console.log('there it is, wooooohooooo!');
-                    }
-    
-                    var currentScroll = $(window).scrollTop();
-                    console.log(currentScroll);
-                    if (currentScroll >= seoDetailsTop) {
+                    if( $(".vx-seo-details-left-column").length > 0 ) {
 
-                        $('.vx-seo-sidebar').css({
-                            position: 'fixed',
-                            top: '0',
-                            right: '0'
-                        });
-                        
-                    } else {
-                        $('.vx-seo-sidebar').css({ 
-                            position: 'static'
+                        var seoDetailsTop = $('.vx-seo-details-left-column').offset().top;
+                        var nextAfterSeoDetails = $('.vx-seo-details-section').next();
+
+                        console.log("next section: " + nextAfterSeoDetails);
+
+                        if ( elementInView( nextAfterSeoDetails ) ) {
+                            //fire at will!
+                            console.log('there it is, wooooohooooo!');
+                        }
+        
+                        var currentScroll = $(window).scrollTop();
+                        console.log(currentScroll);
+                        console.log( "seo details: " + seoDetailsTop);
+                        if (currentScroll >= seoDetailsTop) {
+
+                            $('.vx-seo-sidebar').css({
+                                position: 'fixed',
+                                top: '0',
+                                right: '0'
+                            });
                             
-                        });
-                    }
+                        } else {
+                            $('.vx-seo-sidebar').css({ 
+                                position: 'static'
+                                
+                            });
+                        }
 
-                    // when meet next div reset position
-                    if( currentScroll >= nextAfterSeoDetails ) {
-
-                        $('.vx-seo-sidebar').css({
-                            position: 'static'
-                        });
                     }
     
                 });    
-            }
+            
         }
     });
     
