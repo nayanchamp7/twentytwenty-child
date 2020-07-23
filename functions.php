c7b54d83e87c148efc67ccc8f44144f7b7f4caa2
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-23 17:28:17 +0600

    modify theme table page content

--- functions.php
+++ functions.php
@@ -286,6 +286,23 @@ if( ! function_exists( 'vx_seo_pricing_input' ) ) {
 add_shortcode( 'vx_seo_pricing_input', 'vx_seo_pricing_input' );
 
 
+if( ! function_exists( 'vx_seo_table_quote_btn' ) ) {
+    function vx_seo_table_quote_btn() {
+
+        ob_start();
+
+        if( function_exists( 'get_field' ) ) {
+            $vx_table_btn_text = get_field( 'vx_table_button_text', 'option' );
+            $vx_table_btn_link = get_field( 'vx_table_button_link', 'option' );
+
+            echo sprintf( '<a class="vx-table-btn" href="%">%s</a>', esc_url( $vx_table_btn_link ), esc_html__( $vx_table_btn_text ) );
+        }
+
+        return ob_get_clean();
+    }
+}
+
+
 
 
 
